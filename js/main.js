95e45945d20b01b2337f4c0fcf34e0e78bc1a800
Linda Sysouchanh <linda.sysouchanh@gmail.com>
2017-11-15 21:29:31 -0600

    content added now work on styling

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        fade: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+    var prev = 0;
+    var $window = $(window);
+    var nav = $('.navbar');
+
+    $window.on('scroll', function(){
+        var scrollTop = $window.scrollTop();
+        nav.toggleClass('hidden', scrollTop < prev);
+        prev = scrollTop;
+    });
+    var scrol = 0;
+     $(window).on('scroll', function(){
+        var scrolltop=$(window).scrollTop();
+         $('.nav').toggleClass('hidden', scrolltop > scrol);
+         scrol = scrolltop;
+         console.log('scrol');
+     })
+
+});
